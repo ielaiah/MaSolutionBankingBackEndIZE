a2d492217242d8c0e693e3258b48efa4e01ab31e
imen.zeki <zeki.imen@gmail.com>
2018-10-31 14:28:17 +0100

    Terminaison de l'affichage de la liste des comptes

---
+++ BankingApp.L1.Business/Implementations/ComptesBusinessImpl.cs
@@ -0,0 +1,27 @@
+﻿using BankingApp.L0.DataAccess.Domain;
+using BankingApp.L0.DataAccess.Facade;
+using BankingApp.L0.DataAccess.Implementations;
+using BankingApp.L1.Business.Facade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.L1.Business.Implementations
+{
+    public class ComptesBusinessImpl : IComptesBusiness
+    {
+        private IComptesRepository repository = new ComptesRepositoryImpl_EF();
+        public decimal? ConsulterSolde(string numero)
+        {
+            var resultat = repository.FindById(numero);
+            return resultat?.Solde;
+        }
+
+        public IEnumerable<Compte> TousLesComptes()
+        {
+            return repository.FindAll();
+        }
+    }
+}
